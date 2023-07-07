03d2c94db5af67671dc4c6e25da24f94f1b16646
munich-munster <cmdevacc@gmail.com>
2023-07-07 15:48:09 -0700

    testing running echo script from pre-commit

--- .githooks/areYouSure.sh
+++ .githooks/areYouSure.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+echo "areYouSure.sh ran"
 # Check that we want to commit.
 
-read -p "Are you sure you want to commit this (y/n)? " answer
-case ${answer:0:1} in
-    y|Y )
-        exit 0 # If yes, success!
-    ;;
-    * )
-        exit 1 # If no, sorry yo.
-    ;;
-esac
+# read -p "Are you sure you want to commit this (y/n)? " answer
+# case ${answer:0:1} in
+#     y|Y )
+#         exit 0 # If yes, success!
+#     ;;
+#     * )
+#         exit 1 # If no, sorry yo.
+#     ;;
+# esac
