875ff35e8d1bb8d588985d52a2d2c881499cabbe
munich-munster <cmdevacc@gmail.com>
2023-07-07 16:14:06 -0700

    testing tty in areYouSure.sh

--- .githooks/areYouSure.sh
+++ .githooks/areYouSure.sh
@@ -2,15 +2,16 @@
 
 # Check that we want to commit.
 
+exec < /dev/tty
 read -p "Are you sure you want to commit this (y/n)? " answer
 echo $answer
-# case ${answer:0:1} in
-#     y|Y )
-#         exit 0 # If yes, success!
-#     ;;
-#     * )
-#         exit 1 # If no, sorry yo.
-#     ;;
-# esac
-
-echo "areYouSure.sh ran"
+case ${answer:0:1} in
+    y|Y )
+        echo "areYouSure.sh ran"
+        exit 0 # If yes, success!
+    ;;
+    * )
+        echo "areYouSure.sh ran"
+        exit 1 # If no, sorry yo.
+    ;;
+esac
